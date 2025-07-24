d65df2a6d32e29036300e87ca2b396d687913d44
Lenovo <Lenovo@DESKTOP-FEABINK.mshome.net>
2025-07-24 14:54:42 +0530

    Added new pom class

---
+++ Instagram/src/main/java/com/instagram/ObjectRepositoryUtility/HomePage.java
@@ -0,0 +1,11 @@
+package com.instagram.ObjectRepositoryUtility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+	@FindBy(name = "username")
+	private WebElement usernametxfd;
+
+}
